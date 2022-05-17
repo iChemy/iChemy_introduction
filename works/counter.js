4e5a4dccfb42d5aa0ddd74fc702f97e7df3fb03c
iChemy <controlyuu8@gmail.com>
2022-05-18 01:03:25 +0900

    make a stopwatch

--- works/counter.js
+++ works/counter.js
@@ -1,6 +1,29 @@
 let count = 0
-const countUp = () => {
-    count++
+
+function countShow() {
     const countElement = document.querySelector('#count')
     countElement.innerText = '回数: ' + count
 }
+
+
+const countUp = () => {
+    count++
+    countShow()
+}
+
+const countClear = () => {
+    count = 0
+    countShow()
+}
+
+let time = 0.00
+function timeUp() {
+    time += 0.01
+    const timeElement = document.querySelector('#time')
+    timeElement.innerText = time
+}
+
+const timeStart = () => {
+    setInterval(timeUp, 10);
+}
+
